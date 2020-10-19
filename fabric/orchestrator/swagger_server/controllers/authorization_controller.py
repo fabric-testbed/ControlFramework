5e93750114ea5a805fa45344ad65cd003f720cf1
Komal Thareja <kthare10@renci.org>
2020-10-19 13:51:36 -0400

    fixes for docker deployment

--- fabric/orchestrator/swagger_server/controllers/authorization_controller.py
+++ fabric/orchestrator/swagger_server/controllers/authorization_controller.py
@@ -6,6 +6,6 @@ controller generated to handled auth operation described at:
 https://connexion.readthedocs.io/en/latest/security.html
 """
 def check_bearerAuth(token):
-    return rc.check_bearerAuth()
+    return rc.check_bearerAuth(token=token)
 
 
