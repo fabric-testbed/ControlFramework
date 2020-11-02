94d33b260ffe9dae5d442d9652f9f28b12e40036
Komal Thareja <kthare10@renci.org>
2020-11-02 15:07:00 -0500

    auth fixes

--- fabric/orchestrator/swagger_server/response/authorization_controller.py
+++ fabric/orchestrator/swagger_server/response/authorization_controller.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import connexion
 from fabric.orchestrator.core.orchestrator_handler import OrchestratorHandler
 
 """
@@ -8,6 +8,7 @@ https://connexion.readthedocs.io/en/latest/security.html
 """
 def check_bearerAuth(token):
     orchestrator = OrchestratorHandler()
+    orchestrator.get_logger().log().debug(connexion.request)
     return orchestrator.validate_credentials(token=token)
 
 
