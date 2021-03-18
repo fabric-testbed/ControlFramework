d09d0e0862591d3efa4be650e4567878d0d918e8
Komal Thareja <kthare10@renci.org>
2021-03-17 22:29:19 -0400

    get slivers improved for more readability

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 from fabric_cf import actor
 
 NAME = "fabric-cf"
-VERSION = "0.15"
+VERSION = "0.16"
 # To install the library, run the following
 #
 # python setup.py install
