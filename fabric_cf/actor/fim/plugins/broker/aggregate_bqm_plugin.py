85a06e4181a06a6211ed9a6d4d53388338fe19d3
Ilya Baldin <ibaldin@renci.org>
2021-03-30 15:36:28 -0400

    Adding support for generating links in ABQM; fixing a bug with counting NAS and dataplane switches in capacity

--- fabric_cf/actor/fim/plugins/broker/aggregate_bqm_plugin.py
+++ fabric_cf/actor/fim/plugins/broker/aggregate_bqm_plugin.py
@@ -34,18 +34,25 @@ from fim.graph.resources.abc_bqm import ABCBQMPropertyGraph
 from fim.graph.networkx_property_graph import NetworkXGraphImporter
 from fim.graph.resources.networkx_abqm import NetworkXAggregateBQM
 from fim.slivers.capacities_labels import Capacities
-from fim.slivers.network_node import CompositeNodeSliver
+from fim.slivers.network_node import CompositeNodeSliver, NodeType
 from fim.slivers.attached_components import ComponentSliver
+from fim.slivers.switch_fabric import SFType
+from fim.slivers.interface_info import InterfaceType
 
 
-class AggregateBQMPlugin:
+class AggregatedBQMPlugin:
     """
     Implement a plugin for simple aggregation of CBM into BQM, transforming site
     topologies into CompositeNodes and linking them with (Composite)Links
     This is based on fim.pluggable BrokerPluggable
     """
+    # set to true to test creating ABQM without talking to database or requiring
+    # an actor reference
+    DEBUG_FLAG = False
 
     def __init__(self, actor, logger=None):
+        if not self.DEBUG_FLAG:
+            assert actor is not None
         self.actor = actor
         self.log = logger
 
@@ -57,14 +64,14 @@ class AggregateBQMPlugin:
         :param kwargs:
         :return:
         """
-        if kwargs['query_level'] != 1:
+        if kwargs.get('query_level', None) is None or kwargs['query_level'] != 1:
             return cbm.clone_graph(new_graph_id=str(uuid.uuid4()))
 
         # do a one-pass aggregation of servers, their components and interfaces
         nnodes = cbm.get_all_nodes_by_class(label=ABCPropertyGraph.CLASS_NetworkNode)
         slivers_by_site = defaultdict(list)
         for n in nnodes:
-            # build deep slivers, aggregate by site
+            # build deep slivers for each advertised server, aggregate by site
             node_sliver = cbm.build_deep_node_sliver(node_id=n)
             slivers_by_site[node_sliver.site].append(node_sliver)
 
@@ -72,16 +79,21 @@ class AggregateBQMPlugin:
         abqm = NetworkXAggregateBQM(graph_id=str(uuid.uuid4()),
                                     importer=NetworkXGraphImporter(logger=self.log),
                                     logger=self.log)
-        for s, ls in slivers_by_site.items():
 
+        site_to_composite_node_id = dict()
+        site_to_sf_node_id = dict()
+        for s, ls in slivers_by_site.items():
             # add up capacities and delegated capacities, skip labels for now
             # count up components and figure out links between sites
             site_sliver = CompositeNodeSliver()
             site_sliver.capacities = Capacities()
             site_sliver.resource_name = s
-            # FIXME need a resource type too
+            site_sliver.resource_type = NodeType.Server
             site_comps_by_type = defaultdict(dict)
             for sliver in ls:
+                if sliver.get_type() != NodeType.Server:
+                    # skipping NAS and dataplane switches
+                    continue
                 site_sliver.capacities = site_sliver.capacities + sliver.capacities
                 # collect components in lists by type and model for the site
                 if sliver.attached_components_info is None:
@@ -91,11 +103,21 @@ class AggregateBQMPlugin:
                         site_comps_by_type[comp.resource_type][comp.resource_model] = list()
                     site_comps_by_type[comp.resource_type][comp.resource_model].append(comp)
 
+
             # create a Composite node for every site
             site_node_id = str(uuid.uuid4())
+            site_to_composite_node_id[s] = site_node_id
             site_props = abqm.node_sliver_to_graph_properties_dict(site_sliver)
             abqm.add_node(node_id=site_node_id, label=ABCPropertyGraph.CLASS_CompositeNode,
                           props=site_props)
+            # add a switch fabric
+            sf_id = str(uuid.uuid4())
+            site_to_sf_node_id[s] = sf_id
+            sf_props = {ABCPropertyGraph.PROP_NAME: s + '_sf',
+                        ABCBQMPropertyGraph.PROP_TYPE: str(SFType.SwitchFabric)}
+            abqm.add_node(node_id=sf_id, label=ABCPropertyGraph.CLASS_SwitchFabric, props=sf_props)
+            abqm.add_link(node_a=site_node_id, rel=ABCPropertyGraph.REL_HAS, node_b=sf_id)
+
             # create a component sliver for every component type/model pairing
             # and add a node for it linking back to site node
             for ctype, cdict in site_comps_by_type.items():
@@ -113,5 +135,53 @@ class AggregateBQMPlugin:
                                   props=comp_props)
                     abqm.add_link(node_a=site_node_id, rel=ABCPropertyGraph.REL_HAS,
                                   node_b=comp_node_id)
+
+        # get all intersite links - add them to the aggregated BQM graph
+        intersite_links = cbm.get_intersite_links()
+        for l in intersite_links:
+            source_switch = l[0]
+            sink_switch = l[2]
+            link = l[1]
+            source_site = l[3]
+            sink_site = l[4]
+            source_cp = l[5]
+            sink_cp = l[6]
+            _, cbm_source_cp_props = cbm.get_node_properties(node_id=source_cp)
+            _, cbm_sink_cp_props = cbm.get_node_properties(node_id=sink_cp)
+            _, cbm_link_props = cbm.get_node_properties(node_id=link)
+            # add connection point, link, connection point between to SwitchFabrics
+            assert(site_to_sf_node_id.get(source_site, None) is not None and
+                   site_to_sf_node_id.get(sink_site, None) is not None)
+            source_cp_id = str(uuid.uuid4())
+            sink_cp_id = str(uuid.uuid4())
+            source_cp_props = {ABCPropertyGraph.PROP_NAME: "_".join([source_site, sink_site]),
+                               ABCPropertyGraph.PROP_TYPE: str(InterfaceType.TrunkPort),
+                               ABCPropertyGraph.PROP_CLASS: ABCPropertyGraph.CLASS_ConnectionPoint,
+                               ABCPropertyGraph.PROP_CAPACITIES: cbm_source_cp_props[ABCPropertyGraph.PROP_CAPACITIES]}
+            abqm.add_node(node_id=source_cp_id, label=ABCPropertyGraph.CLASS_ConnectionPoint,
+                          props=source_cp_props)
+            # FIXME: CP names may not be unique if we are dealing with a multigraph
+            sink_cp_props = {ABCPropertyGraph.PROP_NAME: "_".join([sink_site, source_site]),
+                             ABCPropertyGraph.PROP_TYPE: str(InterfaceType.TrunkPort),
+                             ABCPropertyGraph.PROP_CLASS: ABCPropertyGraph.CLASS_ConnectionPoint,
+                             ABCPropertyGraph.PROP_CAPACITIES: cbm_sink_cp_props[ABCPropertyGraph.PROP_CAPACITIES]}
+            abqm.add_node(node_id=sink_cp_id, label=ABCPropertyGraph.CLASS_ConnectionPoint,
+                          props=sink_cp_props)
+            # selectively replicate link node and its properties from CBM
+            new_link_props = {ABCPropertyGraph.PROP_NAME: cbm_link_props[ABCPropertyGraph.PROP_NAME],
+                              ABCPropertyGraph.PROP_TYPE: cbm_link_props[ABCPropertyGraph.PROP_TYPE],
+                              ABCPropertyGraph.PROP_CLASS: cbm_link_props[ABCPropertyGraph.PROP_CLASS],
+                              ABCPropertyGraph.PROP_LAYER: cbm_link_props[ABCPropertyGraph.PROP_LAYER]
+                              }
+            abqm.add_node(node_id=link, label=ABCPropertyGraph.CLASS_Link, props=new_link_props)
+            # connect them together
+            abqm.add_link(node_a=site_to_sf_node_id[source_site], rel=ABCPropertyGraph.REL_CONNECTS,
+                          node_b=source_cp_id)
+            abqm.add_link(node_a=source_cp_id, rel=ABCPropertyGraph.REL_CONNECTS,
+                          node_b=link)
+            abqm.add_link(node_a=link, rel=ABCPropertyGraph.REL_CONNECTS,
+                          node_b=sink_cp_id)
+            abqm.add_link(node_a=sink_cp_id, rel=ABCPropertyGraph.REL_CONNECTS,
+                          node_b=site_to_sf_node_id[sink_site])
 
         return abqm
