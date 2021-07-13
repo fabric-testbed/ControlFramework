69e2bb4340efde8a358f502b0eadf50aed5b4b31
Komal Thareja <kthare10@renci.org>
2021-07-13 10:46:42 -0400

    update fim version and remove hardcoded bw set 0

--- fabric_cf/orchestrator/swagger_server/controllers/authorization_controller.py
+++ fabric_cf/orchestrator/swagger_server/controllers/authorization_controller.py
@@ -1,9 +1,15 @@
 from typing import List
+
+from fabric_cf.actor.security.fabric_token import FabricToken
+
 """
 controller generated to handled auth operation described at:
 https://connexion.readthedocs.io/en/latest/security.html
 """
 def check_bearerAuth(token):
-    return {'test_key': 'test_value'}
+    from fabric_cf.actor.core.container.globals import GlobalsSingleton
+    token = FabricToken(token=token, logger=GlobalsSingleton.get().get_logger())
+    return token.validate()
+
 
 
