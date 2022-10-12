58a97fbe29470ab659b5612eb7715f64a47adbb1
Komal Thareja <kthare10@renci.org>
2022-10-12 12:50:17 -0400

    pass delegation name

--- fabric_cf/actor/core/delegation/broker_delegation.py
+++ fabric_cf/actor/core/delegation/broker_delegation.py
@@ -40,8 +40,8 @@ from fabric_cf.actor.core.util.update_data import UpdateData
 
 
 class BrokerDelegation(Delegation):
-    def __init__(self, dlg_graph_id: str, slice_id: ID, broker: ABCBrokerProxy = None):
-        super().__init__(dlg_graph_id=dlg_graph_id, slice_id=slice_id)
+    def __init__(self, dlg_graph_id: str, slice_id: ID, broker: ABCBrokerProxy = None, delegation_name: str = None):
+        super().__init__(dlg_graph_id=dlg_graph_id, slice_id=slice_id, delegation_name=delegation_name)
         self.exported = False
         self.broker = broker
         self.authority = None
@@ -191,6 +191,7 @@ class BrokerDelegation(Delegation):
         if self.authority is None and incoming.get_site_proxy() is not None:
             self.authority = incoming.get_site_proxy()
 
+        self.delegation_name = incoming.get_delegation_name()
         self.graph = incoming.get_graph()
         self.policy.update_delegation_complete(delegation=self)
         if self.graph is not None:
