55972520a6c993f321e6b05378cbccaa6d5ff153
Komal Thareja <kthare10@renci.org>
2022-11-10 10:15:48 -0500

    pass vm name in interface sliver for port attach

--- fabric_cf/actor/core/kernel/reservation_client.py
+++ fabric_cf/actor/core/kernel/reservation_client.py
@@ -488,7 +488,11 @@ class ReservationClient(Reservation, ABCControllerReservation):
                                                                                 local_name=ifs.get_labels().local_name)
                     parent_labs = parent_res_ifs_sliver.get_label_allocations()
 
-                    ifs.labels = Labels.update(ifs.labels, mac=parent_labs.mac, vlan=parent_labs.vlan)
+                    if component.get_model == "OpenStack-vNIC":
+                        ifs.labels = Labels.update(ifs.labels, mac=parent_labs.mac, vlan=parent_labs.vlan,
+                                                   instance_parent=f"{rid}-{sliver.get_name()}")
+                    else:
+                        ifs.labels = Labels.update(ifs.labels, mac=parent_labs.mac, vlan=parent_labs.vlan)
 
             self.logger.trace(f"Updated Network Res# {self.get_reservation_id()} {sliver}")
 
