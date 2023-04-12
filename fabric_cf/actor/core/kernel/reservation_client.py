44d47fa413b86a1d0d5028435438e60b6121e014
Komal Thareja <kthare10@renci.org>
2023-04-12 10:23:27 -0400

    audit for network services with closed predecessors

--- fabric_cf/actor/core/kernel/reservation_client.py
+++ fabric_cf/actor/core/kernel/reservation_client.py
@@ -1131,16 +1131,13 @@ class ReservationClient(Reservation, ABCControllerReservation):
                     update_data.message = "Redeem/Ticket timeout"
                     self.mark_close_by_ticket_review(update_data=update_data)
 
-        if self.leased_resources is None:
-            return
-
         # Handling for close completion. Note that this reservation could
         # "stick" once we enter the CloseWait state, if we never hear back from
         # the authority. There is no harm to purging a CloseWait reservation,
         # but we just leave them for now.
         close_by_deps = self.__are_dependencies_closed()
-        if (self.pending_state == ReservationPendingStates.Closing and self.leased_resources.is_closed()) or \
-                close_by_deps:
+        if (self.leased_resources is not None and self.pending_state == ReservationPendingStates.Closing and
+            self.leased_resources.is_closed()) or close_by_deps:
 
             if not close_by_deps:
                 self.logger.debug("LEASED RESOURCES are closed")
