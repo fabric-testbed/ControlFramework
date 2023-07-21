9aef666d9d88d1db1f85b4502320f29407a1bc20
Komal Thareja <kthare10@renci.org>
2023-07-21 18:18:34 -0400

    log ssh key hash for add/remove foot print

--- fabric_cf/actor/core/common/event_logger.py
+++ fabric_cf/actor/core/common/event_logger.py
@@ -34,6 +34,7 @@ from fim.user.topology import ExperimentTopology
 from fabric_cf.actor.core.common.constants import Constants
 from fabric_cf.actor.core.common.exceptions import InitializationException
 from fabric_cf.actor.core.util.log_helper import LogHelper
+from fabric_cf.actor.core.util.utils import generate_sha256
 from fabric_cf.actor.security.pdp_auth import ActionId
 
 
@@ -78,7 +79,7 @@ class EventLogger:
             self.logger.error(f"Error occurred: {e}")
             self.logger.error(traceback.format_exc())
 
-    def log_sliver_event(self, *, slice_object: SliceAvro, sliver: BaseSliver, verb: str = None):
+    def log_sliver_event(self, *, slice_object: SliceAvro, sliver: BaseSliver, verb: str = None, keys: list):
         """
         Log Sliver events
         """
@@ -92,8 +93,8 @@ class EventLogger:
             if keys is not None:
                 ssh_foot_print = ""
                 for key_pair in keys:
-                    #fp = get_ssh_key_footprint(public_key_content=key_pair.get('key'))
-                    ssh_foot_print += key_pair.get('key')
+                    fp = generate_sha256(token=key_pair.get('key'))
+                    ssh_foot_print += f":{fp}"
 
             owner = slice_object.get_owner()
             log_message = f"CFEL Sliver event slc:{slice_object.get_slice_id()} " \
@@ -105,6 +106,9 @@ class EventLogger:
             if slice_object.get_config_properties() is not None:
                 token_hash = slice_object.get_config_properties().get(Constants.TOKEN_HASH, "token_hash_not_available")
                 log_message += f":{token_hash}"
+
+            if ssh_foot_print is not None:
+                log_message += f" keys:{ssh_foot_print}"
 
             log_message += f" {str(lc)}"
 
