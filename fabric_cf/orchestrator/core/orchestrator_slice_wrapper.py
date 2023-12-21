eaac168e0f3ed8a94c8c8457a2474e148b2eb840
Komal Thareja <kthare10@renci.org>
2023-12-21 10:47:51 -0500

    changes for logging

--- fabric_cf/orchestrator/core/orchestrator_slice_wrapper.py
+++ fabric_cf/orchestrator/core/orchestrator_slice_wrapper.py
@@ -148,10 +148,11 @@ class OrchestratorSliceWrapper:
             self.controller.add_reservation(reservation=r)
         self.logger.info(f"ADD TIME: {time.time() - start:.0f}")
 
-    def create(self, *, slice_graph: ABCASMPropertyGraph) -> List[LeaseReservationAvro]:
+    def create(self, *, slice_graph: ABCASMPropertyGraph, topology: ExperimentTopology) -> List[LeaseReservationAvro]:
         """
         Create a slice
         :param slice_graph: Slice Graph
+        :param topology: Experiment Topology
         :return: List of computed reservations
         """
         try:
@@ -176,6 +177,7 @@ class OrchestratorSliceWrapper:
                 self.computed_add_reservations.append(r)
                 self.computed_reservations.append(r)
 
+            self.__update_topology(topology=topology)
             return self.computed_reservations
         except OrchestratorException as e:
             self.logger.error("Exception occurred while generating reservations for slivers: {}".format(e))
@@ -454,7 +456,13 @@ class OrchestratorSliceWrapper:
             sliver_to_res_mapping[nn_id] = reservation.get_reservation_id()
         return reservations, sliver_to_res_mapping
 
-    def modify(self, *, new_slice_graph: ABCASMPropertyGraph) -> List[LeaseReservationAvro]:
+    def modify(self, *, new_slice_graph: ABCASMPropertyGraph, topology: ExperimentTopology) -> List[LeaseReservationAvro]:
+        """
+        Modify an existing slice
+        :param new_slice_graph New Slice Graph
+        :param topology Experiment Topology
+        :return: List of computed reservations
+        """
         existing_topology = FimHelper.get_experiment_topology(graph_id=self.slice_obj.get_graph_id())
 
         new_topology = ExperimentTopology()
@@ -605,6 +613,7 @@ class OrchestratorSliceWrapper:
         for x in modified_reservations:
             self.computed_reservations.append(x)
 
+        self.__update_topology(topology=topology)
         return self.computed_reservations
 
     def __check_modify_on_fabnetv4ext(self, *, rid: str, req_sliver: NetworkServiceSliver) -> NetworkServiceSliver:
@@ -679,3 +688,17 @@ class OrchestratorSliceWrapper:
             req_sliver.reservation_info.error_message = "IP Addresses were updated due to conflicts"
 
         return req_sliver
+
+    def __update_topology(self, *, topology: ExperimentTopology):
+        for x in self.computed_reservations:
+            sliver = x.get_sliver()
+            node_name = sliver.get_name()
+            if isinstance(sliver, NodeSliver) and node_name in topology.nodes:
+                node = topology.nodes[node_name]
+                node.set_properties(labels=sliver.labels,
+                                    label_allocations=sliver.label_allocations,
+                                    capacity_allocations=sliver.capacity_allocations,
+                                    reservation_info=sliver.reservation_info,
+                                    node_map=sliver.node_map,
+                                    management_ip=sliver.management_ip,
+                                    capacity_hints=sliver.capacity_hints)
