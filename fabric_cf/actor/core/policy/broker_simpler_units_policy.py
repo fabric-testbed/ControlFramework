9f4ba2b5626121c420d0a46aa3481b287f92cb1b
Komal Thareja <kthare10@renci.org>
2024-07-09 23:45:16 -0400

    update owner_ns_id for extend

--- fabric_cf/actor/core/policy/broker_simpler_units_policy.py
+++ fabric_cf/actor/core/policy/broker_simpler_units_policy.py
@@ -823,7 +823,11 @@ class BrokerSimplerUnitsPolicy(BrokerCalendarPolicy):
             bqm_graph_id, bqm_node_id = sliver.get_node_map()
             owner_ns, owner_switch = self.get_network_service_from_graph(node_id=bqm_node_id,
                                                                          parent=True)
-            owner_ns_id = owner_ns_id.node_id
+            # Hack for IPV6Ext services
+            owner_ns_id = owner_ns.node_id
+            if 'ipv6ext-ns' in owner_ns_id:
+                owner_ns_id = owner_ns_id.replace('ipv6ext-ns', 'ipv6-ns')
+
             owner_mpls_ns = None
             if owner_switch:
                 for ns in owner_switch.network_service_info.network_services.values():
